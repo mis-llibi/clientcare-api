75bab23946a28f45cba7cbf3eb5d14246a5237a0
0ojireho0 <jeremiahquintano17@gmail.com>
2026-08-11 09:50:35 +0800

    Update DesktopClientRequestController.php

--- app/Http/Controllers/ClientCare/DesktopClientRequestController.php
+++ app/Http/Controllers/ClientCare/DesktopClientRequestController.php
@@ -342,7 +342,7 @@ class DesktopClientRequestController extends Controller
         $petrn_plan = [01,02,03,04];
 
         // PETRON EXCLUSION IN CHSI
-        if(in_array($findPatient->plan, $petrn_plan) && $provider_id == 1608){
+        if($findPatient->company_code == "PETRN" && in_array($findPatient->plan, $petrn_plan) && $provider_id == 1608){
             return response()->json([
                 'message' => 'This provider is excluded to your plan.'
             ], 404);
